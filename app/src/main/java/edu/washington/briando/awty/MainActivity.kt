07d85afe65f642b6e9a0ee69bf186be52e1c3a18
Brian Do <thebriando@gmail.com>
2019-05-21 16:15:45 -0700

    Implemented SMS for part 2

--- app/src/main/java/edu/washington/briando/awty/MainActivity.kt
+++ app/src/main/java/edu/washington/briando/awty/MainActivity.kt
@@ -1,9 +1,20 @@
 package edu.washington.briando.awty
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.widget.*
 import android.text.*
+import android.telephony.SmsManager
+import android.util.Log
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
+
+private const val TAG = "awtydebug"
+private const val REQUEST_SMS_SEND_PERMISSION = 1234
 
 class MainActivity : AppCompatActivity() {
     private lateinit var message: EditText
@@ -11,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nag: EditText
     private lateinit var btn: Button
     private lateinit var toast: Toast
+    private lateinit var timer: Timer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,11 +50,15 @@ class MainActivity : AppCompatActivity() {
                     btn.text = "Start"
                 } else {
                     toast = Toast.makeText(this, "Texting $toastPhone : $toastMsg", Toast.LENGTH_SHORT)
+                    if (this.checkPermissions()) {
+                        sendSMS(toastPhone, toastMsg, interval)
+                    }
                 }
                 toast.show()
             } else {
                 btn.text = "Start"
                 toast.cancel()
+                timer.cancel()
             }
         }
     }
@@ -94,5 +110,36 @@ class MainActivity : AppCompatActivity() {
         if (phone.text.isNotEmpty() && nag.text.isNotEmpty() && message.text.isNotEmpty()) {
             btn.isEnabled = true
         }
+    }
+
+    private fun sendSMS(toastPhone: String, toastMsg: String, interval: String) {
+        Log.d(TAG, "sending sms")
+        val smsManager = SmsManager.getDefault()
+        timer = fixedRateTimer("timer", false, 0L, interval.toLong() * 1000) {
+            this@MainActivity.runOnUiThread {
+                Log.d(TAG, "here")
+                smsManager.sendTextMessage(
+                    toastPhone,
+                    null,
+                    toastMsg,
+                    null,
+                    null
+                )
+            }
+        }
+    }
+
+
+    private fun checkPermissions(): Boolean {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.SEND_SMS),
+                REQUEST_SMS_SEND_PERMISSION
+            )
+            return false
+        }
+        return true
     }
 }
